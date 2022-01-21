4e58115bc539fcc5a64bc932adf452783f8b9958
Yana Mishkina <mishkina_yana@mail.ru>
2022-01-21 17:02:29 +0300

    fix: классы кнопок по БЭМ

--- scripts/index.js
+++ scripts/index.js
@@ -1,12 +1,12 @@
-let profileEditButtonOpen = document.querySelector('.button__profile_size_s');
+let profileEditButtonOpen = document.querySelector('.button_size_s');
 let popup = document.querySelector('.popup');
-let popupCloseButton = document.querySelector('.button__popup_type_resert');
+let popupCloseButton = document.querySelector('.button_type_resert');
 let formElement = document.querySelector('.popup__form');
 let nameInput = document.querySelector('.popup__field_value_name');
 let jobInput = document.querySelector('.popup__field_value_job');
 let profileTitle = document.querySelector('.profile__title');
 let profileSubtitle = document.querySelector('.profile__subtitle');
-let submitButton = document.querySelector('.button__popup_type_submit');
+let submitButton = document.querySelector('.button_type_submit');
 
 function openPopup() {
   popup.classList.add('popup_opened');
