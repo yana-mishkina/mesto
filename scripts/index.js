169b1cef04667d745a36629edcfb6db4c74524e4
Yana Mishkina <mishkina_yana@mail.ru>
2022-01-22 12:40:28 +0300

    fix: классы и стили кнопок

--- scripts/index.js
+++ scripts/index.js
@@ -1,24 +1,21 @@
-let profileEditButtonOpen = document.querySelector('.button_size_s');
+let profileEditButtonOpen = document.querySelector('.profile__button_size_s');
 let popup = document.querySelector('.popup');
-let popupCloseButton = document.querySelector('.button_type_resert');
+let popupCloseButton = document.querySelector('.popup__button_type_resert');
 let formElement = document.querySelector('.popup__form');
 let nameInput = document.querySelector('.popup__field_value_name');
 let jobInput = document.querySelector('.popup__field_value_job');
 let profileTitle = document.querySelector('.profile__title');
 let profileSubtitle = document.querySelector('.profile__subtitle');
-let submitButton = document.querySelector('.button_type_submit');
+let submitButton = document.querySelector('.popup__button_type_submit');
 
 function openPopup() {
   popup.classList.add('popup_opened');
+  nameInput.value = profileTitle.textContent;
+  jobInput.value = profileSubtitle.textContent;
 }
 
 function closePopup() {
   popup.classList.remove('popup_opened');
-}
-
-function unsavePopup () {
-  nameInput.value = profileTitle.textContent;
-  jobInput.value = profileSubtitle.textContent;
 }
 
 function formSubmitHandler (evt) {
@@ -29,6 +26,5 @@ function formSubmitHandler (evt) {
 
 profileEditButtonOpen.addEventListener('click', openPopup);
 popupCloseButton.addEventListener('click', closePopup);
-profileEditButtonOpen.addEventListener('click', unsavePopup);
 formElement.addEventListener('submit', formSubmitHandler); 
 submitButton.addEventListener('click', closePopup);
